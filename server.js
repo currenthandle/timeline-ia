4bb133af78d3fcc5f01505d2ab91f721879505e5
leptone <leptone55@gmail.com>
2016-04-26 15:32:17 -0700

    fixed port

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ var server = http.createServer(function (req, res){
 	st(req, res)  // else look in static files
 })
 
-server.listen(5000 || process.ENV.PORT)
+server.listen(process.ENV.PORT || 5000)
